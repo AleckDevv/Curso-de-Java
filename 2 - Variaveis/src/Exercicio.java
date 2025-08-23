efa83c89e578a6032226bf96dc75d541d400aeb9
Alex Soares Lopes <as3434126@gmail.com>
2025-08-22 22:25:38 -0300

    add aula sobre variáveis

---
+++ 2 - Variaveis/src/Exercicio.java
@@ -0,0 +1,19 @@
+public class Exercicio {
+    public static void main(String[] args) {
+        /*
+         * primeira forma: primeiro inicializar e depois atribui o valor
+         */
+        String nome;
+        nome = "Alex";
+        System.out.println(nome);
+
+        /*
+         * segunda forma: inicializa e já atribui o valor direto
+         */
+        int idade = 25;
+        System.out.println(idade);
+
+        double salario = 2500;
+        System.out.println(salario);
+    }
+}
